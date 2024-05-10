3ec44e83a82e8b3a5c32fd3f8307d7847187a43e
hasansezertasan <hasansezertasan@gmail.com>
2024-05-10 09:44:54 +0300

    More install and import commands added.

--- scripts/installer.ps1
+++ scripts/installer.ps1
@@ -1,17 +1,27 @@
 # Windows Installer
 
-echo Installing Scoop
+echo - Installing Scoop
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-echo Instlaling pyenv-win
+echo - Instlaling pyenv-win
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
 Remove-Item -Path "./install-pyenv-win.ps1"
 
-echo Installing Rye - Not Supported on Windows via PS yet
+echo - Installing Rye - Not Supported on Windows via PS yet
 
-echo Install PipX Packages
-Get-Content pipx-requirements.txt | ForEach-Object { pipx install $_ }
+echo - Install PipX Packages
+Get-Content pipx-requirements.txt | ForEach-Object {
+    $package = $_.Split(" ")[0]
+    $version = $_.Split(" ")[1]
+    pipx install $package==$version
+}
 
-echo Install Pip Requirements
+echo - Install Pip Requirements
 pip install -r requirements.txt
+
+echo - Install Scoop Packages
+scoop import scoop/scoop.json
+
+echo - Install Winget Packages
+winget import winget/winget.json
