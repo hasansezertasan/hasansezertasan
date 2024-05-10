eb3c1f6c9f84c3f7975a590c224b8beee438de61
hasansezertasan <hasansezertasan@gmail.com>
2024-05-10 09:45:36 +0300

    Exporter script parallel to installer.

---
+++ scripts/exporter.ps1
@@ -0,0 +1,16 @@
+# Windows Exporter
+
+echo - Exporting Scoop Packages
+scoop export > scoop/scoop.json
+
+echo - Exporting Winget Packages
+winget export winget/winget.json
+
+echo - Exporting pyenv # TODO
+
+echo - Exporting PipX Packages
+pipx list --short > pipx-requirements.txt
+
+echo - Exporting Pip Requirements
+pip freeze > pip-requirements.txt
+
