6818946888ae7ee4934ee49cf75d934a0fe645c3
hasansezertasan <hasansezertasan@gmail.com>
2024-05-10 08:49:37 +0300

    Installer for both scoop and pyenv-win on windows via PS Script.

---
+++ scripts/installer.ps1
@@ -0,0 +1,11 @@
+# Windows Installer
+
+echo Installing Scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+echo Instlaling pyenv-win
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+Remove-Item -Path "./install-pyenv-win.ps1"
+
+echo Installing Rye - Not Supported on Windows via PS yet
