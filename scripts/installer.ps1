b6df65750ee19b394d923e587b48576142391b14
hasansezertasan <hasansezertasan@gmail.com>
2024-06-10 05:44:23 +0300

    Install hatch via script, instead of pip.

--- scripts/installer.ps1
+++ scripts/installer.ps1
@@ -4,7 +4,12 @@ echo - Installing Scoop
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-echo - Instlaling pyenv-win
+echo - Installing Hatch
+$winVer = (Get-WmiObject -Class Win32_OperatingSystem).OSArchitecture
+$winVer = $winVer -replace "(\d+)(.*)", '$1'
+msiexec /passive /i https://github.com/pypa/hatch/releases/latest/download/hatch-$winVer.msi
+
+echo - Installing pyenv-win
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
 Remove-Item -Path "./install-pyenv-win.ps1"
 
